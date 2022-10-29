569560c3169515c1db3f32f0d5db2bf640fca218
Mahesh Singh <singh.mahesh@gmail.com>
2022-10-29 08:05:04 +0530

    125. Valid Palindrome https://leetcode.com/problems/valid-palindrome/

--- two-pointers/valid-palindrome/valid_palindrome.go
+++ two-pointers/valid-palindrome/valid_palindrome.go
@@ -1,6 +1,5 @@
 /*
-https://leetcode.com/problems/valid-palindrome/
-
+125. Valid Palindrome https://leetcode.com/problems/valid-palindrome/
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false otherwise.
@@ -25,39 +24,47 @@ Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 */
 
+/*
+Notes
+- Checking is alpha-numeric, need to create a func based on rune value (0-9 == 48 -57) and (a-z == 97-122)
+- Make string to lower by strings.ToLower()
+- convert to lower string into []rune
+- Two pointer, one start from start called left and another from end called right,
+- if left or right pointer char is not a alpha-num, increase/decrease the pointer and contunue the loop
+- otherwise check the equality between left pointer char and right pointer char. if they are not equeal return false
+*/
+
 package valid_palindrome
 
 import (
 	"strings"
-	"unicode/utf8"
 )
 
 func isPalindrome(s string) bool {
-
 	if len(s) == 0 {
 		return true
 	}
-
-	left, right := 0, utf8.RuneCountInString(s)-1
 	s = strings.ToLower(s)
+	chars := []rune(s)
+	left, right := 0, len(chars)-1
+
 	for left < right {
 		if !isAlphaNum(s[left]) {
-			//skip the char
-			left = left + 1
+			left++
+			continue
 		}
 
 		if !isAlphaNum(s[right]) {
-			right = right - 1
+			right--
+			continue
 		}
 
 		if isAlphaNum(s[left]) && isAlphaNum(s[right]) && s[left] != s[right] {
 			return false
 		}
 
-		if isAlphaNum(s[left]) && isAlphaNum(s[right]) && s[left] == s[right] {
-			left = left + 1
-			right = right - 1
-		}
+		left++
+		right--
 	}
 
 	return true
